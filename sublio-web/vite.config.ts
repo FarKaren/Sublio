3b0e62f4873e03e622e606b867e0c5f25eedd2a8
Karen Farmanyan <farmanian.k@yandex.ru>
2026-07-05 12:09:14 +0300

    chore(FAR-42): bundle analysis & performance optimization

--- sublio-web/vite.config.ts
+++ sublio-web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import * as path from "node:path";
 
 // https://vite.dev/config/
@@ -9,7 +10,8 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    visualizer({ open: true, gzipSize: true })
   ],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') }
